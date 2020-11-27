d14f3dcec01c11a735f55eec8764e2c9916e3193
Duy Khánh <duykhanh.sctn@gmail.com>
2020-11-27 19:00:53 +0700

    xxx

---
+++ SLGIS.Web/wwwroot/js/map-init.js
@@ -0,0 +1,16 @@
+﻿// Initialize and add the map
+function initMap() {
+    // The location of Hanoi
+    const hanoi = { lat: 21.026613, lng: 105.833889 };
+
+    // The map, centered at hanoi
+    const map = new google.maps.Map(document.getElementById("content"), {
+        zoom: 8,
+        center: hanoi,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: hanoi,
+        map: map,
+    });
+}
