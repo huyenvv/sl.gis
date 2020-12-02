bf97fb6b09d70dfb661d0745df117e7b6a64edd3
huyenvv90 <huyenvv90@gmail.com>
2020-12-02 08:29:42 +0700

    PageModelBase

---
+++ SLGIS.Web/Model/PageModelBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using SLGIS.Core;
+using SLGIS.Implementation;
+
+namespace SLGIS.Web
+{
+    public class PageModelBase : PageModel
+    {
+        private readonly HydropowerService _hydropowerService;
+        public PageModelBase(HydropowerService hydropowerService)
+        {
+            _hydropowerService = hydropowerService;
+        }
+
+        public HydropowerPlant GetCurrentHydropower()
+        {
+            return _hydropowerService.GetCurrent(User.GetId()).GetAwaiter().GetResult();
+        }
+    }
+}
