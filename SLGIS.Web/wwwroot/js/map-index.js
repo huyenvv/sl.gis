be7aa8683237016e7e9e4f25fa4212a08b8303c8
Unknown <duykhanh.sctn@gmail.com>
2020-12-06 14:32:18 +0700

    xxx

--- SLGIS.Web/wwwroot/js/map-index.js
+++ SLGIS.Web/wwwroot/js/map-index.js
@@ -17,6 +17,31 @@
     $('#search-marker').select2({
         placeholder: 'Tìm kiếm'
     });
-    $('input[name=layerSelect]').click(showMarkers);
+    $('input[name=layerSelect]').click(function () {
+        (function ($element) {
+            checkSubstations($element);
+            showMarkers();
+        }($(this)));
+    });
+
     $('.go-back').click(() => $('.info').each(function () { $(this).addClass('d-none') }));
+
+    function checkSubstations($element) {
+        if ($element.val() == 'substation') {
+            if ($element.is(':checked')) {
+                $('.electric-level').each(function () { $(this).prop('checked', true); });
+            }
+            else {
+                $('.electric-level').each(function () { $(this).prop('checked', false); });
+            }
+        }
+        if ($element.hasClass('electric-level')) {
+            if ($('.electric-level').length === $('.electric-level:checked').length) {
+                $('#chkLine').prop('checked', true);
+            }
+            else {
+                $('#chkLine').prop('checked', false);
+            }
+        }
+    }
 })(jQuery);
