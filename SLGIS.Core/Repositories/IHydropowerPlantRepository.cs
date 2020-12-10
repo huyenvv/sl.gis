4031039722901cc13c5bcabd4a68ca19fd551c56
Duy Khánh <duykhanh.sctn@gmail.com>
2020-12-10 18:19:04 +0700

    xxx

--- SLGIS.Core/Repositories/IHydropowerPlantRepository.cs
+++ SLGIS.Core/Repositories/IHydropowerPlantRepository.cs
@@ -1,8 +1,11 @@
 ﻿using SLGIS.Core.Model;
+using System;
+using System.Threading.Tasks;
 
 namespace SLGIS.Core
 {
     public interface IHydropowerPlantRepository : IBaseRepository<HydropowerPlant>
     {
+        Task SetLocationAsync(Guid id, Location location);
     }
 }
--- SLGIS.Implementation/Repositories/HydropowerPlantRepository.cs
+++ SLGIS.Implementation/Repositories/HydropowerPlantRepository.cs
@@ -1,5 +1,7 @@
 ﻿using SLGIS.Core;
 using MongoDB.Driver;
+using System.Threading.Tasks;
+using System;
 
 namespace SLGIS.Implementation
 {
@@ -7,6 +9,11 @@ namespace SLGIS.Implementation
     {
         public HydropowerPlantRepository(IMongoDatabase db) : base(db)
         {
+        }
+        public Task SetLocationAsync(Guid id, Location location)
+        {
+            var filter_id = Builders<HydropowerPlant>.Filter.Eq("_id", id);
+            return _collection.UpdateOneAsync(filter_id, Builders<HydropowerPlant>.Update.Set(m => m.Location, location));
         }
     }
 }
